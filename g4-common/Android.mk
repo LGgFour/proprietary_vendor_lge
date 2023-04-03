ae201a9a149b9bce119c55a2d300bc671141e567
steadfasterX <steadfasterX@gmail.com>
2023-04-03 12:24:46 +0200

    dos fixes

--- g4-common/Android.mk
+++ g4-common/Android.mk
@@ -43,8 +43,8 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_ds_api
 LOCAL_MODULE_OWNER := lge
-LOCAL_SRC_FILES_64 := proprietary/lib64/libloc_ds_api.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libloc_ds_api.so
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libloc_ds_api.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libloc_ds_api.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
